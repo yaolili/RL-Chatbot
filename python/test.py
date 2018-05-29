a24d62da4febdc423f9f574f946b55a18bae24b7
yaolili <yaolili12235@gmail.com>
2018-05-29 14:44:20 +0800

    update test

--- python/test.py
+++ python/test.py
@@ -1,41 +1,40 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+"""
+    single turn conversation test
+"""
+
 
 from __future__ import print_function
 
+print('testing')
+
+''' get graphic card first '''
+from sess_factory import SESS
+
+''' import python library '''
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
-import cPickle as pkl
-
-from gensim.models import KeyedVectors
-import data_parser
-import config
-
-from model import Seq2Seq_chatbot
 import tensorflow as tf
-import numpy as np
-
-import re
-import sys
+from gensim.models import KeyedVectors
 import time
 
-import utils
+''' import our utils, config and model '''
+from utils import make_batch_X
+import data_parser
 from data_reader import Data_Reader
-from utils import make_batch_X, make_batch_Y, index2sentence  # x
+import config
 
-sess = tf.InteractiveSession()
-
-#=====================================================
+# =====================================================
 # Global Parameters
-#=====================================================
-default_model_path = config.test_model_path
-testing_data_path = config.test_data_path if len(sys.argv) <= 2 else sys.argv[2]
-output_path = config.test_out_path if len(sys.argv) <= 3 else sys.argv[3]
-
+# =====================================================
+model_path = os.path.join(config.model_path, config.checkpoint)
+testing_data_path = config.test_data_path
+output_path = config.test_out_path
 word_count_threshold = config.WC_threshold
 
-#=====================================================
+# =====================================================
 # Train Parameters
-#=====================================================
+# =====================================================
 dim_wordvec = config.dim_wordvec
 dim_hidden = config.dim_hidden
 
@@ -44,64 +43,93 @@ n_decode_lstm_step = config.n_decode_lstm_step
 
 batch_size = config.batch_size
 
-def test(model_path=default_model_path):
 
+def test():
+    """
+
+    :return: no return
+    """
+    ''' load dictionary '''
     word_vector = KeyedVectors.load_word2vec_format(config.pretrain_emb, binary=True)
+    w2i, i2w, bias_init_vector = data_parser.preProBuildWordVocab(word_count_threshold=word_count_threshold)
 
-    wordtoix, ixtoword, bias_init_vector = data_parser.preProBuildWordVocab(word_count_threshold=word_count_threshold)
-
-    model = config.test_model_proto(
+    ''' build tf model, saver '''
+    if 'alpha' in config.model_type:
+        model = config.model_proto(
             dim_wordvec=dim_wordvec,
-            n_words=len(ixtoword),
+            n_words=len(i2w),
+            dim_hidden=dim_hidden,
+            batch_size=batch_size,
+            n_encode_lstm_step=n_encode_lstm_step,
+            n_decode_lstm_step=n_decode_lstm_step,
+            bias_init_vector=bias_init_vector,
+            ex_batch=1)
+    else:
+        model = config.model_proto(
+            dim_wordvec=dim_wordvec,
+            n_words=len(i2w),
             dim_hidden=dim_hidden,
             batch_size=batch_size,
             n_encode_lstm_step=n_encode_lstm_step,
             n_decode_lstm_step=n_decode_lstm_step,
             bias_init_vector=bias_init_vector)
-
-    word_vectors, caption_tf, probs, _ = model.build_generator()
-
-    global sess
+    model.build_generator()
 
     saver = tf.train.Saver()
-    try:
-        print('\n=== Use model', model_path, '===\n')
-        saver.restore(sess, model_path)
-    except:
-        print('\nUse default model\n')
-        saver.restore(sess, default_model_path)
+    print('\n=== Use model', model_path, '===\n')
+    saver.restore(SESS, model_path)
 
-    with open(output_path, 'w') as fout:
-        generated_sentences = []
-        bleu_score_avg = [0., 0.]
-        dr = Data_Reader(config.test_data_path)
-        n_batch = dr.get_batch_num(batch_size)
-        for batch in range(n_batch):
-            batch_X, batch_Y = dr.generate_training_batch(batch_size)
-            feats = make_batch_X(batch_X, n_encode_lstm_step, dim_wordvec, word_vector)
-            for idx, (x, y) in enumerate(zip(batch_X, batch_Y)):
-                    
-                generated_words_index, prob_logit = sess.run([caption_tf, probs], feed_dict={word_vectors: [feats[idx]]})
-            
-                # print("test query: ", x)
-                # print("golden reply: ", y)
-                sent = ""
-                for index in generated_words_index:
-                    sent += ixtoword[index].decode("utf-8")
-                # print("Generated reply: ", sent.encode('utf-8'))
-                # print("***********************")
-                fout.write("test query: " + x)
-                fout.write("\n")
-                fout.write("golden reply: " + y)
-                fout.write("\n")
-                fout.write("Generated reply: " + sent.encode('utf-8'))
-                fout.write("\n")
-                fout.write("***********************")
-                fout.write("\n")
+    ''' open read and write path '''
+    dr = Data_Reader(testing_data_path)
+    n_batch = dr.get_batch_num(batch_size)
+
+    query_out = open(output_path + '.query', 'w')
+    reference_out = open(output_path + '.reference', 'w')
+    generate_out = open(output_path + '.generate', 'w')
+
+    ''' running batches '''
+    start_time = time.time()
+
+    for batch in range(n_batch):
+
+        ''' build data '''
+        batch_x, batch_y, _, kw = dr.generate_training_batch_with_former(batch_size)
+        if config.model_type == 'rev':
+            tmp = batch_x
+            batch_x = batch_y
+            batch_y = tmp
+        feats = make_batch_X(batch_x, n_encode_lstm_step, dim_wordvec, word_vector)
+
+        generated_words_index, _ = model.test(SESS, (feats, kw), word_vector)
+
+        for idx, (x, y, gw) in enumerate(zip(batch_x, batch_y, generated_words_index)):
+
+            words = []
+            for index in gw:
+                if index == 2:
+                    break
+                words.append(i2w[index].decode("utf-8"))
+            sent = ' '.join(words)
+
+            query_out.write(x)  # print with \n, but file is not, maybe wb and w different
+            query_out.write("\n")
+
+            reference_out.write(y)
+            reference_out.write("\n")
+
+            generate_out.write(sent.encode('utf-8'))
+            generate_out.write("\n")
+
+            cnt = batch_size * batch + idx
+            if cnt % 1000 == 0:
+                print(cnt)
+
+    query_out.close()
+    reference_out.close()
+    generate_out.close()
+
+    print('cost time: {}'.format(time.time() - start_time))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        test(model_path=sys.argv[1])
-    else:
-        test()
+    test()
