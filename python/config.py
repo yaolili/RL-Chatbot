48b75ed02f1c43ae056511f9edec06414e8fc0b6
yaolili <yaolili12235@gmail.com>
2018-06-12 17:29:37 +0800

    remove backup for rl

--- python/config.py
+++ python/config.py
@@ -22,7 +22,7 @@ parser.add_argument('-c', '--checkpoint', help='use which checkpoint for test or
 parser.add_argument('-d', '--debug_on', help='turn on to use debug mode (使输出数据位置和运行模式完全隔离，避免混淆)',
                     action='store_true')
 parser.add_argument('-m', '--model_type', help='type of model',
-                    choices=['env', 'seq2seq', 'kw_sample', TYPE_OF_OUR_MODEL], default='env')
+                    choices=['env', 'seq2seq', 'rev', 'kw_sample', TYPE_OF_OUR_MODEL], default='env')
 parser.add_argument('-p', '--print_every', help='output message every N batch',
                     type=int, default=50)
 parser.add_argument('-s', '--n_step', help=TYPE_OF_OUR_MODEL + '\'s generation turns (config会自行修正，不用手动减1了)',
@@ -67,14 +67,10 @@ empty_kw = '<empty>'.decode('utf-8')  # representation of no keyword
 '''
 model import selection
 '''
-if model_type == 'seq2seq' or model_type == 'rl' or model_type == 'rev':
+if model_type == 'seq2seq' or model_type == 'rev':
     import model
 
     model_proto = model.Seq2Seq_chatbot
-elif model_type == 'kw':
-    import kw_model
-
-    model_proto = kw_model.Kw_chatbot
 elif model_type == 'kw_sample':
     import kw_sample_model
 
